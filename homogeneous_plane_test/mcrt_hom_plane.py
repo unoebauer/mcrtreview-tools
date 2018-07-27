700944c6fc8742f03cb3fdf5f7de30aa5cd23046
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2018-07-27 10:06:54 +0200

    Add progress bar, clean-up codebase

--- homogeneous_plane_test/mcrt_hom_plane.py
+++ homogeneous_plane_test/mcrt_hom_plane.py
@@ -1,10 +1,22 @@
 from __future__ import print_function
+import sys
 import numpy as np
-import astropy.units as u
-import astropy.constants as c
 
 
 np.random.seed(0)
+
+
+def progress(count, total, suffix=''):
+    # See
+    # https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
+    sys.stdout.flush()
 
 
 class mc_packet(object):
@@ -23,7 +35,6 @@ class mc_packet(object):
 
         self.x = self.grid.xl[i] + self.grid.dx[i] * np.random.rand(1)[0]
         self.mu = 2. * np.random.rand(1)[0] - 1.
-        self.tau = -np.log(np.random.rand(1)[0])
 
         self.l_edge = self.calculate_distance_edge()
         self.l_int = self.calculate_distance_interaction()
@@ -35,6 +46,7 @@ class mc_packet(object):
 
     def calculate_distance_interaction(self):
 
+        self.tau = -np.log(np.random.rand(1)[0])
         return self.tau / self.cell_chi
 
     def calculate_distance_edge(self):
@@ -48,16 +60,11 @@ class mc_packet(object):
 
         return dx / self.mu
 
-    def update_position(self, l):
-
-        return self.x + l * self.mu
-
     def update_estimators(self, l):
 
         self.grid.Jestimator[self.cell_index] = \
             self.grid.Jestimator[self.cell_index] + \
             l * self.L / (4. * np.pi * self.cell_dx)
-            # l * self.L / (4. * np.pi * self.cell_dx * self.grid.dt)
         self.grid.Hestimator[self.cell_index] = \
             self.grid.Hestimator[self.cell_index] + \
             l * self.mu * self.L / (4. * np.pi * self.cell_dx)
@@ -70,13 +77,6 @@ class mc_packet(object):
         self.update_estimators(self.l_int)
         self.is_absorbed = True
 
-        # self.x = self.update_position(self.l_int)
-        # self.mu = 2. * np.random.rand(1)[0] - 1.
-
-        # self.tau = -np.log(np.random.rand(1)[0])
-        # self.l_edge = self.calculate_distance_edge()
-        # self.l_int = self.calculate_distance_interaction()
-
     def change_cell(self):
 
         self.update_estimators(self.l_edge)
@@ -86,36 +86,32 @@ class mc_packet(object):
             self.is_escaped = True
             self.x = self.cell_xr
 
-            return False
-
-        if self.next_cell_index == -1:
+        elif self.next_cell_index == -1:
 
             self.x = self.cell_xl
             self.mu = -self.mu
 
-            self.tau = -np.log(np.random.rand(1)[0])
             self.l_edge = self.calculate_distance_edge()
             self.l_int = self.calculate_distance_interaction()
 
-            return True
-
-        if self.next_cell_index > self.cell_index:
-
-            self.x = self.grid.xl[self.next_cell_index]
-
         else:
 
-            self.x = self.grid.xr[self.next_cell_index]
+            if self.next_cell_index > self.cell_index:
 
-        self.cell_index = self.next_cell_index
-        self.cell_chi = self.grid.chi[self.cell_index]
-        self.cell_xl = self.grid.xl[self.cell_index]
-        self.cell_xr = self.grid.xr[self.cell_index]
-        self.cell_dx = self.grid.dx[self.cell_index]
+                self.x = self.grid.xl[self.next_cell_index]
 
-        self.tau = -np.log(np.random.rand(1)[0])
-        self.l_edge = self.calculate_distance_edge()
-        self.l_int = self.calculate_distance_interaction()
+            else:
+
+                self.x = self.grid.xr[self.next_cell_index]
+
+            self.cell_index = self.next_cell_index
+            self.cell_chi = self.grid.chi[self.cell_index]
+            self.cell_xl = self.grid.xl[self.cell_index]
+            self.cell_xr = self.grid.xr[self.cell_index]
+            self.cell_dx = self.grid.dx[self.cell_index]
+
+            self.l_edge = self.calculate_distance_edge()
+            self.l_int = self.calculate_distance_interaction()
 
     def propagate(self):
 
@@ -143,7 +139,6 @@ class mcrt_grid(object):
 
         self.Ncells = Ncells
         self.Npackets = Npackets
-        # self.dt = 1e-10
 
         dx = xmax / float(self.Ncells)
         self.xl = np.arange(self.Ncells) * dx
@@ -154,28 +149,18 @@ class mcrt_grid(object):
         self.eta = np.where(self.xr <= xint, S * chi, 1e-20)
 
         self.Ltot = 4. * np.pi * np.sum(self.eta * self.dx)
-        # self.Ltot = np.sum(self.eta * self.dx * self.dt)
         self.L = self.Ltot / float(self.Npackets)
 
         self.npackets_cell = (4. * np.pi * self.eta * self.dx /
                               self.L).astype(np.int)
-        # self.npackets_cell = (self.eta * self.dx * self.dt / self.L).astype(np.int)
-        self.npackets_cell_cum = np.cumsum(self.npackets_cell)
-        self.npackets_cell_cum_frac = \
-            np.cumsum(self.npackets_cell).astype(np.float) / np.sum(self.npackets_cell)
+        self.npackets_cell_cum_frac = (
+            np.cumsum(self.npackets_cell).astype(np.float) /
+            np.sum(self.npackets_cell))
         self.packets = []
-
-        # j = 0
-        # for i in range(self.Npackets):
-        #     if not i < self.npackets_cell_cum[j]:
-        #         j = j+1
-
-        #     self.packets.append(mc_packet(j, self, self.L))
 
         self.Jestimator = np.zeros(self.Ncells)
         self.Hestimator = np.zeros(self.Ncells)
         self.Kestimator = np.zeros(self.Ncells)
-        print(self.npackets_cell_cum_frac)
 
     def propagate(self):
 
@@ -185,83 +170,15 @@ class mcrt_grid(object):
         self.esc_packets_mu = []
         self.esc_packets_L = []
 
-        for i in range(N):
-
+        for j in range(N):
             z = np.random.rand(1)[0]
             i = np.argwhere((self.npackets_cell_cum_frac - z) > 0)[0, 0]
             packet = mc_packet(i, self, self.L)
-            packet.propagate()
+            ret = packet.propagate()
+            assert(ret)
             if packet.is_escaped:
                 self.esc_packets_x.append(packet.x)
                 self.esc_packets_mu.append(packet.mu)
                 self.esc_packets_L.append(packet.L)
 
-        # while 1:
-        #     if N == 0:
-        #         print("Success")
-        #         break
-        #     if n > 10000000:
-        #         print("Failure")
-        #         break
-        #     z = np.random.rand(1)[0]
-        #     i = np.argwhere((self.npackets_cell_cum_frac - z) > 0)[0, 0]
-        #     packet = mc_packet(i, self, self.L)
-        #     print("Initializing packet in cell {:d}".format(i))
-        #     packet.propagate()
-        #     if packet.is_escaped:
-        #         print("packet escaped")
-        #         N = N - 1
-        #     elif packet.is_absorbed:
-        #         print("packet packet was absorbed")
-        #     else:
-        #         raise ValueError("Something went wrong")
-        #     n = n + 1
-        #     print("{:d} packets processed".format(n))
-        #     print("{:d} packets escaped".format(self.Npackets - N))
-
-
-        # self.Jestimator = self.Jestimator * self.Npackets / float(n)
-        # self.Hestimator = self.Hestimator * self.Npackets / float(n)
-        # self.Kestimator = self.Kestimator * self.Npackets / float(n)
-
-        # for i, packet in enumerate(self.packets):
-
-        #     print(i)
-        #     packet.propagate()
-
-
-
-
-
-# class simulator(object):
-#     def __init__(self, chi=1, eta=1, L1=10, L2=20, N=10000, Ncells=100):
-#
-#         self.N = N
-#
-#         self.dx = L2 / float(Ncells)
-#         self.xl = np.arange(Ncells) * self.dx
-#         self.xr = self.xl + self.dx
-#
-#         self.chi = np.where(self.xr <= L2, chi, 1e-20)
-#         self.eta = np.where(self.xr <= L2, eta, 1e-20)
-#
-#         self.Ltot = np.sum(self.eta * self.dx)
-#         self.L = self.Ltot / float(N)
-#
-#         self.npackets_cell = (self.Ltot / self.L).astype(np.int)
-#         self.npackets_cell_cum = np.cumsum(self.npackets_cell)
-#         self.packets_cell_index = np.zeros(self.N)
-#
-#         for i in range(self.N):
-#             self.packets_cell_index[i] = \
-#                 np.argwhere(i < self.npackets_cell_cum)[0, 0]
-#
-#         self.packets_xl = self.packets_cell_index * self.dx
-#         self.packets_xr = self.packets_xl + self.dx
-#
-#         self.packets_x = np.random.rand(self.N) * self.dx + self.packets_xl
-#         self.packets_L = self.L * np.ones(self.N)
-#         self.packets_mu = np.random.rand(self.N) * 2. - 1.
-#         self.packets_tau = -np.log(np.random.rand(self.N))
-#
-#         self.packets_is_active = np.ones(self.N).astype(np.bool)
+            progress(j, N, suffix='')
