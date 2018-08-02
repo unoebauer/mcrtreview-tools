cfe7397ea71cef1d16cad6ce648b54e3c160fe28
Ulrich Noebauer <ulrich.noebauer@gmail.com>
2018-08-02 09:55:34 +0200

    Make mcrt tool modular
    * move all geometry related concepts to mixin classes
    * move all generic properties to base classes

--- homogeneous_plane_test/mcrt_hom_plane.py
+++ homogeneous_plane_test/mcrt_hom_plane.py
@@ -19,7 +19,7 @@ def progress(count, total, suffix=''):
     sys.stdout.flush()
 
 
-class mc_packet(object):
+class mc_packet_base(object):
 
     def __init__(self, i, grid, L):
 
@@ -29,6 +29,7 @@ class mc_packet(object):
         self.cell_xl = self.grid.xl[i]
         self.cell_xr = self.grid.xr[i]
         self.cell_dx = self.grid.dx[i]
+        self.cell_dV = self.grid.dV[i]
         self.cell_chi = self.grid.chi[i]
 
         self.L = L
@@ -49,6 +50,64 @@ class mc_packet(object):
         self.tau = -np.log(np.random.rand(1)[0])
         return self.tau / self.cell_chi
 
+    def update_estimators(self, l):
+
+        self.grid.Jestimator[self.cell_index] = \
+            self.grid.Jestimator[self.cell_index] + \
+            l * self.L / (4. * np.pi * self.cell_dV)
+        self.grid.Hestimator[self.cell_index] = \
+            self.grid.Hestimator[self.cell_index] + \
+            l * self.mu * self.L / (4. * np.pi * self.cell_dV)
+        self.grid.Kestimator[self.cell_index] = \
+            self.grid.Kestimator[self.cell_index] + \
+            l * self.mu**2 * self.L / (4. * np.pi * self.cell_dV)
+
+    def interact(self):
+
+        self.update_estimators(self.l_int)
+        self.is_absorbed = True
+
+    def propagate(self):
+
+        i = 0
+        while 1:
+            if i > self.prop_cycle_limit:
+                print("Cycle Limit reached")
+                return False
+            if self.is_escaped or self.is_absorbed:
+                return True
+
+            if self.l_int < self.l_edge:
+
+                self.interact()
+            else:
+
+                self.change_cell()
+
+            i = i+1
+
+
+class mc_packet_spherical_geom_mixin(object):
+
+    def initialize_position(self):
+
+        pass
+
+    def calculate_distance_edge(self):
+
+        pass
+
+    def change_cell(self):
+
+        pass
+
+
+class mc_packet_planar_geom_mixin(object):
+
+    def initialize_position(self):
+
+        self.x = self.cell_xl + self.cell_dx * np.random.rand(1)[0]
+
     def calculate_distance_edge(self):
 
         if self.mu > 0:
@@ -59,23 +118,6 @@ class mc_packet(object):
             self.next_cell_index = self.cell_index - 1
 
         return dx / self.mu
-
-    def update_estimators(self, l):
-
-        self.grid.Jestimator[self.cell_index] = \
-            self.grid.Jestimator[self.cell_index] + \
-            l * self.L / (4. * np.pi * self.cell_dx)
-        self.grid.Hestimator[self.cell_index] = \
-            self.grid.Hestimator[self.cell_index] + \
-            l * self.mu * self.L / (4. * np.pi * self.cell_dx)
-        self.grid.Kestimator[self.cell_index] = \
-            self.grid.Kestimator[self.cell_index] + \
-            l * self.mu**2 * self.L / (4. * np.pi * self.cell_dx)
-
-    def interact(self):
-
-        self.update_estimators(self.l_int)
-        self.is_absorbed = True
 
     def change_cell(self):
 
@@ -113,32 +155,34 @@ class mc_packet(object):
             self.l_edge = self.calculate_distance_edge()
             self.l_int = self.calculate_distance_interaction()
 
-    def propagate(self):
 
-        i = 0
-        while 1:
-            if i > self.prop_cycle_limit:
-                print("Cycle Limit reached")
-                return False
-            if self.is_escaped or self.is_absorbed:
-                return True
+class mc_packet_planar(mc_packet_base, mc_packet_planar_geom_mixin):
+    def __init__(self, i, grid, L):
 
-            if self.l_int < self.l_edge:
-
-                self.interact()
-
-            else:
-                self.change_cell()
-
-            i = i+1
+        super(mc_packet_planar, self).__init__(i, grid, L)
 
 
-class mcrt_grid(object):
+class mc_packet_spherical(mc_packet_base, mc_packet_spherical_geom_mixin):
+    def __init__(self, i, grid, L):
+
+        super(mc_packet_spherical, self).__init__(i, grid, L)
+
+
+class mcrt_grid_base(object):
     def __init__(self, chi=2.5e-4, S=10., xint=1e6, xmax=5e6, Ncells=100,
                  Npackets=1000000):
 
         self.Ncells = Ncells
         self.Npackets = Npackets
+
+        self.packets = []
+        self.esc_packets_x = []
+        self.esc_packets_mu = []
+        self.esc_packets_L = []
+
+        self.Jestimator = np.zeros(self.Ncells)
+        self.Hestimator = np.zeros(self.Ncells)
+        self.Kestimator = np.zeros(self.Ncells)
 
         dx = xmax / float(self.Ncells)
         self.xl = np.arange(self.Ncells) * dx
@@ -148,32 +192,25 @@ class mcrt_grid(object):
         self.chi = np.where(self.xr <= xint, chi, 1e-20)
         self.eta = np.where(self.xr <= xint, S * chi, 1e-20)
 
-        self.Ltot = 4. * np.pi * np.sum(self.eta * self.dx)
+    def determine_number_of_packets(self):
+
+        self.Ltot = 4. * np.pi * np.sum(self.eta * self.dV)
         self.L = self.Ltot / float(self.Npackets)
 
-        self.npackets_cell = (4. * np.pi * self.eta * self.dx /
+        self.npackets_cell = (4. * np.pi * self.eta * self.dV /
                               self.L).astype(np.int)
         self.npackets_cell_cum_frac = (
             np.cumsum(self.npackets_cell).astype(np.float) /
             np.sum(self.npackets_cell))
-        self.packets = []
-
-        self.Jestimator = np.zeros(self.Ncells)
-        self.Hestimator = np.zeros(self.Ncells)
-        self.Kestimator = np.zeros(self.Ncells)
 
     def propagate(self):
 
         N = self.Npackets
 
-        self.esc_packets_x = []
-        self.esc_packets_mu = []
-        self.esc_packets_L = []
-
         for j in range(N):
             z = np.random.rand(1)[0]
             i = np.argwhere((self.npackets_cell_cum_frac - z) > 0)[0, 0]
-            packet = mc_packet(i, self, self.L)
+            packet = self.init_packet(i)
             ret = packet.propagate()
             assert(ret)
             if packet.is_escaped:
@@ -182,3 +219,48 @@ class mcrt_grid(object):
                 self.esc_packets_L.append(packet.L)
 
             progress(j, N, suffix='')
+
+
+class mcrt_grid_planar_geom_mixin(object):
+
+    def determine_cell_volume(self):
+
+        self.dV = self.dx.copy()
+
+    def init_packet(self, i):
+
+        return mc_packet_planar(i, self, self.L)
+
+    def determine_analytic_solution(self):
+
+        pass
+
+
+class mcrt_grid_spherical_geom_mixin(object):
+
+    def determine_cell_volume(self):
+
+        pass
+
+    def init_packet(self, i):
+
+        pass
+
+    def determine_analytic_solution(self):
+
+        pass
+
+
+class mcrt_grid_planar(mcrt_grid_base, mcrt_grid_planar_geom_mixin):
+
+    def __init__(self, chi=2.5e-4, S=10., xint=1e6, xmax=5e6, Ncells=100,
+                 Npackets=1000000):
+
+        super(mcrt_grid_planar, self).__init__(chi=chi, S=S, xint=xint,
+                                               xmax=xmax, Ncells=Ncells,
+                                               Npackets=Npackets)
+
+        self.determine_cell_volume()
+        self.determine_number_of_packets()
+
+        self.propagate()
